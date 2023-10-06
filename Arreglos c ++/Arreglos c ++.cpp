797dff181d401eea89a1da90027dd87565efd857
Rosa-Gonzalez35 <145619608+Rosa-Gonzalez35@users.noreply.github.com>
2023-10-06 16:14:31 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos c ++/Arreglos c ++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int numeros[6] = { 10,20,30,40,50,60 };
+	cout << numeros[5] << endl;
+
+	int n[3] = { 3,4,5 };
+	cout << n[2] << endl;
+
+	char c[50] = { 'L','u','i','s' };
+	cout << c << endl;
+
+	char s[50] = "Mona Lisa";
+	cout << s << endl;
+
+	return 0;
+}
